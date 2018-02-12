b1e73e8b6f05be9e984e88957e1839d4d246dae9
Gambleruin <dannylovewargame@gmail.com>
2018-02-11 20:45:07 -0400

    *

--- Apriori.py
+++ Apriori.py
@@ -1,49 +1,49 @@
 import csv
-
 class Transaction:
     def __init__(self, items):
         self.items = set(items)
 
-
-# for get all item function, there is a one line lambda expression
-flatten = lambda l: [item for sublist in l for item in sublist]
 class Apriori:
     def __init__(self, list_transactions):
+        self.flatten = lambda l: [item for sublist in l for item in sublist]
         self.transactions = list_transactions
         self.all_items = set()
         for trn in self.transactions:
-            self.all_items |= trn.items
+            self.all_items |=trn.items
         pass
 
     def get_support(self, item):
         # !!! Write code to return support of set_items
-        N =len(transactions)
         count =0
-        for e in transactions:
-            if (e == item):
+        N= len(self.all_items)
+        '''
+        for i in self.transactions:
+            print (i.items)
+        '''
+        for i in range(len(self.transactions)):
+            if item in self.transactions[i].items:
                 count =count +1
-
         return count/N
+        
         pass
-   
+
     def get_combinations(self, items, num):
+        # !!! Write code to return "num" number of combinations of items
+        item_list =list(items)
         if num:
             for i in range(num -1, len(items)):
-                for cn in get_combinations(items[:i], num -1):
-                    yield cn +(items[i], )
+                for cn in self.get_combinations(item_list[:i], num -1):
+                    yield cn +(item_list[i],)
         else:
-            yield tuple()
+            yield tuple()  
         pass
 
     def get_all_items(self, itemsets):
-        # !!! Write code to merge all the itemsets and return the current items
-        # all_items =[item for sublist in test_data for item in sublist]
-        return flatten(itemsets)
+        return self.flatten(itemsets)
         pass
 
-    # using magical bitmap
     def is_bit_set(num, bit):
-        return num&(1 <<bit) > 0
+        return num&(1 <<bit) >0
 
     def get_subsets(self, items):
         # !!! Write code to return all subsets of "items"
@@ -55,12 +55,31 @@ class Apriori:
         pass
 
     def get_rules(self, min_sup, min_con):
-        var_a = self.get_combinations(self.all_items, 1)
+        # print(self.all_items)
+        list_tuple = list(self.get_combinations(self.all_items, 1))
+        var_a =[list(elem) for elem in list_tuple]
+
+        print(var_a)
+        print(self.transactions[0].items)
         num = 2
         freq_items = []
         supports = []
+
+        a =var_a[0]
+        print(a)
+        
+        print(type(var_a[0]))
+        print(self.get_support(a[0]))
+        
+        
+
+        # var_b = list(map(lambda x: self.get_support(x), var_a))
+        # print(var_b)
+'''
         while True:
-            var_b = map(lambda x: self.get_support(x), var_a)
+            print(var_a)
+            var_b = list(map(lambda x: self.get_support(x), var_a))
+            print(var_b)
             chosen_itemset = []
             chosen_supports = []
             for index, items in enumerate(var_a):
@@ -74,13 +93,13 @@ class Apriori:
                 break
             var_a = self.get_combinations(var_c, num)
             num += 1
-        print "Frequent Itemsets"
+        print ("Frequent Itemsets")
         for item, support in sorted(zip(freq_items, supports), key=lambda x: x[1]):
-            print ",".join(item), support
+            print (",".join(item), support)
 
         rules = []
         confidences = []
-        print "Rules"
+        print ("Rules")
         for items in freq_items:
             subsets = self.get_subsets(items)
             support = self.get_support(items)
@@ -93,8 +112,8 @@ class Apriori:
                     confidences.append(support / support_A)
 
         for rule, confidence in sorted(zip(rules, confidences), key=lambda x: x[1]):
-            print ",".join(rule[0]), "=>", ",".join(rule[1]), confidence
-
+            print (",".join(rule[0]), "=>", ",".join(rule[1]), confidence)
+'''
 if __name__ == "__main__":
     test_data = [
         ['bread', 'milk'],
@@ -105,18 +124,16 @@ if __name__ == "__main__":
     ]
 
     transactions = []
-    test = False
+    test = True
 
     if test:
         for row in test_data:
-            transactions.append(Transaction(set(row)))
+            transactions.append(Transaction(row))
     else:
         with open("apriori_data.csv", "r") as f:
             for row in csv.reader(f):
-                transactions.append(Transaction(set(row)))
+                transactions.append(Transaction(row))
 
     a = Apriori(transactions)
     a.get_rules(min_sup=0.15, min_con= 0.6)
-
-
 
