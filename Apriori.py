ec9c5827a9fc74773f4c557b567f837be347690c
Gambleruin <dannylovewargame@gmail.com>
2018-02-12 00:59:45 -0400

    *

--- Apriori.py
+++ Apriori.py
@@ -1,4 +1,5 @@
 import csv
+import pdb
 class Transaction:
     def __init__(self, items):
         self.items = set(items)
@@ -16,12 +17,10 @@ class Apriori:
         # !!! Write code to return support of set_items
         count =0
         N= len(self.all_items)
-        '''
-        for i in self.transactions:
-            print (i.items)
-        '''
+        item_ ={elem for elem in item}
+
         for i in range(len(self.transactions)):
-            if item in self.transactions[i].items:
+            if item_ < self.transactions[i].items:
                 count =count +1
         return count/N
         
@@ -39,47 +38,34 @@ class Apriori:
         pass
 
     def get_all_items(self, itemsets):
-        return self.flatten(itemsets)
+        fat_list =self.flatten(itemsets)
+        all_items =set(fat_list)
+        return all_items
         pass
 
-    def is_bit_set(num, bit):
+    def is_bit_set(self, num, bit):
         return num&(1 <<bit) >0
 
     def get_subsets(self, items):
         # !!! Write code to return all subsets of "items"
         sets =[]
         for i in range(1 <<len(items)):
-            subset =[items[bit] for bit in range(len(items)) if is_bit_set(i, bit)]
+            subset =[items[bit] for bit in range(len(items)) if self.is_bit_set(i, bit)]
             sets.append(subset)
-        return sets
+
+        # the empty list better to be removed
+        subsets =[x for x in sets if x !=[]]
+        return subsets
         pass
 
     def get_rules(self, min_sup, min_con):
-        # print(self.all_items)
-        list_tuple = list(self.get_combinations(self.all_items, 1))
-        var_a =[list(elem) for elem in list_tuple]
-
-        print(var_a)
-        print(self.transactions[0].items)
+        var_a = list(self.get_combinations(self.all_items, 1))
         num = 2
         freq_items = []
         supports = []
 
-        a =var_a[0]
-        print(a)
-        
-        print(type(var_a[0]))
-        print(self.get_support(a[0]))
-        
-        
-
-        # var_b = list(map(lambda x: self.get_support(x), var_a))
-        # print(var_b)
-'''
         while True:
-            print(var_a)
             var_b = list(map(lambda x: self.get_support(x), var_a))
-            print(var_b)
             chosen_itemset = []
             chosen_supports = []
             for index, items in enumerate(var_a):
@@ -91,29 +77,56 @@ class Apriori:
             var_c = self.get_all_items(chosen_itemset)
             if num > len(var_c):
                 break
-            var_a = self.get_combinations(var_c, num)
+            var_a = list(self.get_combinations(var_c, num))
             num += 1
-        print ("Frequent Itemsets")
+            # print(num, len(var_c))
+            
+         
+        print('the loop is over')   
+        # print ("Frequent Itemsets")
+        '''
         for item, support in sorted(zip(freq_items, supports), key=lambda x: x[1]):
             print (",".join(item), support)
+        '''
 
         rules = []
         confidences = []
-        print ("Rules")
+        # print ("Rules")
+        # print(var_a, '\n')
+        # print(freq_items, '\n','\n\n')
+
+
+        '''
+        for item in freq_items:
+            print(item)
+            subsets = self.get_subsets(item)
+            print('\n', subsets)
+
+        '''     
         for items in freq_items:
+            items_ =set(items)
             subsets = self.get_subsets(items)
+            # print(subsets_)
+            # print(subsets, '\n')
             support = self.get_support(items)
             for var_d in subsets:
-                if len(items - var_d) == 0:
+                var_d_ =set(var_d)
+                # var_d_ =self.get_all_items(var_d)
+                # print(var_d_, items_)
+                if len(items_ - var_d_) == 0:
                     continue
-                support_A = self.get_support(var_d)
+                support_A = self.get_support(var_d_)
+
                 if support / support_A > min_con:
-                    rules.append((var_d, items - var_d))
+                    rules.append((var_d_, items_ - var_d_))
                     confidences.append(support / support_A)
 
         for rule, confidence in sorted(zip(rules, confidences), key=lambda x: x[1]):
             print (",".join(rule[0]), "=>", ",".join(rule[1]), confidence)
-'''
+
+        
+
+
 if __name__ == "__main__":
     test_data = [
         ['bread', 'milk'],
